c045ff43df136101b8e8acea95761fe2879926f1
lupin <tubin.rajan@gmail.com>
2022-12-20 21:32:47 +0530

    bug fixed

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.TodoListview.as_view(),name='todofn'),
+    path('',views.todofn,name='todofn'),
+
     path('delete/<int:tid>/',views.deletefn,name='deletefn'),
-    path('edit/<int:taskid>/',views.update,name='update'),
+    path('edit/<int:pk>/',views.update ,name='update'),
    
    
 ]
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,25 +1,59 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from .models import Task
 from .forms import TodoForm
 from django.views.generic import ListView
-
-class TodoListview(ListView):
-    model=Task
-    template_name='home.html'
-    context_object_name= 'task1'
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
 
+# class todocreate(CreateView):
+#     model= Task
+#     # template_name= 'home.html'
+    
+#     fields = ['name','priority','date']
+#     sucess_url = reverse_lazy('todofn')
+    
 
-# def todofn(request):
-#     if request.method == 'POST':
-#         name=request.POST.get('name','')
-#         priority=request.POST.get('priority','')
-#         date=request.POST.get('date','')
-#         task=Task(name=name,priority=priority,date=date)
-#         task.save()
-#     task1=Task.objects.all()
+
+# class TodoListview(ListView):
+#     model=Task
+#     template_name='home.html'
+#     context_object_name= 'task1'
+    
+    
+    
+# class Todoupdate(UpdateView):
+#     model= Task
+#     template_name='edit.html'
+#     context_object_name='task'
+#     fields= ['name','priority','date']
+#     def get_success_url(self) :
+#         return reverse_lazy('todofn')
+#         # ,kwargs={'pk':self.object.id}
+    
+
+# class tododelete(DeleteView):
+#     model= Task
+#     template_name= 'delete.html'
+#     sucess_url = reverse_lazy('todofn')
+
+# class TodoDetail(DetailView):
+    # model = Task
+    # template_name = 'home.html'
+    # context_object_name = 'task1'
+
+
+def todofn(request):
+    if request.method == 'POST':
+        name=request.POST.get('name','')
+        priority=request.POST.get('priority','')
+        date=request.POST.get('date','')
+        task=Task(name=name,priority=priority,date=date)
+        task.save()
+    task1=Task.objects.all()
         
-#     return render(request,'home.html',{'task1':task1})
+    return render(request,'home.html',{'task1':task1})
 # Create your views here.
 def deletefn(request,tid):
     task=Task.objects.get(id=tid)
